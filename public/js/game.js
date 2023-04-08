f396b25479a5342d5ac1960af5a0207719a3bbcf
Owen Palmer <speedcubegeek@gmail.com>
2023-04-08 03:12:14 -0700

    New node button and camera fix

--- public/js/game.js
+++ public/js/game.js
@@ -23,6 +23,7 @@ function preload() {
     this.load.image('otherPlayer', 'assets/enemyBlack5.png');
     this.load.image('node', 'assets/node.png');
     this.load.image('check', 'assets/check.png');
+    this.load.image('new_node', 'assets/new_node.png');
 }
 
 
@@ -39,12 +40,12 @@ function create() {
     this.socket = io();
     this.socket.on('playerMoved', function (playerInfo) {
         self.otherPlayers.getChildren().forEach(function (otherPlayer) {
-          if (playerInfo.playerId === otherPlayer.playerId) {
-            otherPlayer.setRotation(playerInfo.rotation);
-            otherPlayer.setPosition(playerInfo.x, playerInfo.y);
-          }
+            if (playerInfo.playerId === otherPlayer.playerId) {
+                otherPlayer.setRotation(playerInfo.rotation);
+                otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+            }
         });
-      });
+    });
     this.otherPlayers = this.physics.add.group();
     this.socket.on('currentPlayers', function (players) {
         Object.keys(players).forEach(function (id) {
@@ -67,7 +68,7 @@ function create() {
     });
     this.socket.on('nodeAdded', function (nodeData) {
         // create new node object 
-        var newNode = {x: nodeData.x, y: nodeData.y, color: nodeData.y, text: nodeData.text};
+        var newNode = { x: nodeData.x, y: nodeData.y, color: nodeData.y, text: nodeData.text };
         console.log(newNode);
         // nodes.push(newNode);
         drawNode(self, nodeData.text, newNode);
@@ -77,7 +78,17 @@ function create() {
 
     let bg = this.add.image(0, 0, "sky").setOrigin(0, 0);
     camera = this.cameras.main;
-
+    // let button = this.add.sprite(0, 0, 'node');
+    // button.setInteractive();
+    // button.on('pointerdown', function () {
+    //     console.log('asdf');
+    // });
+    new_post = this.add.sprite(0, 0, 'new_node');
+    new_post.setOrigin(0.0, 0.0).setDisplaySize(180,100);
+    new_post.setInteractive();
+    new_post.on('pointerdown', function () {
+        console.log('asdfjkl');
+    });
 }
 function addPlayer(self, playerInfo) {
     self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
@@ -89,7 +100,9 @@ function addPlayer(self, playerInfo) {
     self.ship.setDrag(100);
     self.ship.setAngularDrag(100);
     self.ship.setMaxVelocity(200);
-    camera.startFollow(self.ship);
+    // camera.startFollow(self.ship);
+    // camera.follow(self.ship);
+    // camera.followOffset.set(self.ship.x, self.ship.y);
     camera.setZoom(0.8);
 }
 function addOtherPlayers(self, playerInfo) {
@@ -103,12 +116,12 @@ function addOtherPlayers(self, playerInfo) {
     self.otherPlayers.add(otherPlayer);
 }
 
-function drawNode(self, str, node){
+function drawNode(self, str, node) {
     font_size = 16;
     wrap_width = 19;
 
     console.log(self.ship.x);
-    height = (Math.ceil(str.length/wrap_width) * 18) + 18;
+    height = (Math.ceil(str.length / wrap_width) * 18) + 18;
     sprite = self.add.sprite(node.x, node.y, 'node').setDisplaySize(200, height);
     sprite.inputEnabled = true;
 
@@ -120,53 +133,56 @@ function drawNode(self, str, node){
         // chunks.push(str.substring(i, i + chunkSize));
         chunk = str.substring(i, i + wrap_width);
         // console.log(chunk);
-        text = self.add.text(node.x-80, (node.y-(height/2))+(line*font_size), chunk, style);
+        text = self.add.text(node.x - 80, (node.y - (height / 2)) + (line * font_size), chunk, style);
     }
     // this.socket.emit('newNode', {x: this.ship.x, y: this.ship.y, color: this.ship.team, text: 'hello'})
 }
 
 function update() {
     if (this.ship) {
-        // console.log(this.keys);
+        new_post.x = camera.scrollX - 100;
+        new_post.y = camera.scrollY + camera.height - 30;
+
+        const targetX = this.ship.x - (camera.width / 2);
+        const targetY = this.ship.y - (camera.height / 2);
+        lerp = 0.3;
+        camera.scrollX = lerp * targetX + (1 - lerp) * camera.scrollX;
+        camera.scrollY = lerp * targetY + (1 - lerp) * camera.scrollY;
+
         if (this.keys.new_node._justDown) {
             textarea.style.display = 'block';
         }
         if (this.keys.create_node._justDown) {
             textarea.style.display = 'none';
-            // drawNode(this, "test");
-
-            this.socket.emit('newNode', {x: this.ship.x, y: this.ship.y, color: this.ship.team, text: textarea.value})
+            this.socket.emit('newNode', { x: this.ship.x, y: this.ship.y, color: this.ship.team, text: textarea.value })
         }
 
+        speed = 5;
         if (this.cursors.left.isDown) {
-            //this.ship.setAngularVelocity(-150);
-            this.ship.x -= 5
-        } 
-        
+            this.ship.x -= speed;
+        }
+
         if (this.cursors.right.isDown) {
-            //this.ship.setAngularVelocity(150);
-            this.ship.x += 5
-        } 
+            this.ship.x += speed;
+        }
 
         if (this.cursors.up.isDown) {
-            //this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
-            this.ship.y -= 6    
-        } 
-        
-        if (this.cursors.down.isDown) {
-            //this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
-            this.ship.y += 6    
-        } 
+            this.ship.y -= speed;
+        }
 
-        if (this.keys.ZoomIn.isDown){
+        if (this.cursors.down.isDown) {
+            this.ship.y += speed;
+        }
+
+        if (this.keys.ZoomIn.isDown) {
             camera.zoom += 0.04
             console.log("want to draw");
-           // let graphics = this.add.graphics();
-           // graphics.lineBetween(x1, y1, x2, y2);
+            // let graphics = this.add.graphics();
+            // graphics.lineBetween(x1, y1, x2, y2);
             //drawLine(0,0,500,500);
         }
-        
-        if (this.keys.ZoomOut._justDown){
+
+        if (this.keys.ZoomOut._justDown) {
             camera.zoom -= 0.04;
         }
 
