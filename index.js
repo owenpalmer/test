104ada8185fba6b876e868191e36c4c2423b738e
Owen Palmer <speedcubegeek@gmail.com>
2023-04-08 00:58:17 -0700

    yay

--- index.js
+++ index.js
@@ -3,40 +3,49 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var players = {};
+var nodes = [];
 app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 io.on('connection', function (socket) {
-  console.log('a user connected');
-  // create a new player and add it to our players object
-  players[socket.id] = {
-    rotation: 0,
-    x: Math.floor(Math.random() * 700) + 50,
-    y: Math.floor(Math.random() * 500) + 50,
-    playerId: socket.id,
-    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
-  };
-  // send the players object to the new player
-  socket.emit('currentPlayers', players);
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
-  // when a player disconnects, remove them from our players object
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-    // remove this player from our players object
-    delete players[socket.id];
-    // emit a message to all players to remove this player
-    io.emit('disconnect', socket.id);
-  });
-  socket.on('playerMovement', function (movementData) {
-    players[socket.id].x = movementData.x;
-    players[socket.id].y = movementData.y;
-    players[socket.id].rotation = movementData.rotation;
-    // emit a message to all players about the player that moved
-    socket.broadcast.emit('playerMoved', players[socket.id]);
-  });
+    console.log('a user connected');
+    // create a new player and add it to our players object
+    players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id,
+        team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+    };
+    // send the players object to the new player
+    socket.emit('currentPlayers', players);
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+    // when a player disconnects, remove them from our players object
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        // remove this player from our players object
+        delete players[socket.id];
+        // emit a message to all players to remove this player
+        io.emit('disconnect', socket.id);
+    });
+    socket.on('playerMovement', function (movementData) {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        players[socket.id].rotation = movementData.rotation;
+        // emit a message to all players about the player that moved
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
+    socket.on('newNode', function (nodeData) {
+        // create new node object 
+        var newNode = { x: nodeData.x, y: nodeData.y, color: nodeData.y, text: nodeData.text };
+        console.log(newNode);
+        nodes.push(newNode);
+        // emit a message to all players a new node was added
+        socket.broadcast.emit('nodeAdded', { x: nodeData.x, y: nodeData.y, color: nodeData.y, text: nodeData.text });
+    });
 });
 server.listen(8081, function () {
-  console.log(`Listening on ${server.address().port}`);
+    console.log(`Listening on ${server.address().port}`);
 });
--- public/js/game.js
+++ public/js/game.js
@@ -17,12 +17,24 @@ var config = {
     }
 };
 var game = new Phaser.Game(config);
+// var nodes = [];
 function preload() {
     this.load.image('ship', 'assets/spaceShips_001.png');
     this.load.image('otherPlayer', 'assets/enemyBlack5.png');
+    this.load.image('node', 'assets/node.png');
+    this.load.image('check', 'assets/check.png');
 }
 
+
 function create() {
+    textarea = document.querySelector('textarea');
+    this.keys = this.input.keyboard.addKeys({
+        // space: Phaser.Input.Keyboard.KeyCodes.SPACE,
+        new_node: Phaser.Input.Keyboard.KeyCodes.ONE,
+        create_node: Phaser.Input.Keyboard.KeyCodes.TWO,
+        ZoomIn: Phaser.Input.Keyboard.KeyCodes.NINE,
+        ZoomOut: Phaser.Input.Keyboard.KeyCodes.EIGHT
+    });
     var self = this;
     this.socket = io();
     this.socket.on('playerMoved', function (playerInfo) {
@@ -53,7 +65,19 @@ function create() {
             }
         });
     });
+    this.socket.on('nodeAdded', function (nodeData) {
+        // create new node object 
+        var newNode = {x: nodeData.x, y: nodeData.y, color: nodeData.y, text: nodeData.text};
+        console.log(newNode);
+        nodes.push(newNode);
+        drawNode(self, nodeData.text);
+        // emit a message to all players a new node was added
+    });
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    let bg = this.add.image(0, 0, "sky").setOrigin(0, 0);
+    camera = this.cameras.main;
+
 }
 function addPlayer(self, playerInfo) {
     self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
@@ -65,6 +89,8 @@ function addPlayer(self, playerInfo) {
     self.ship.setDrag(100);
     self.ship.setAngularDrag(100);
     self.ship.setMaxVelocity(200);
+    camera.startFollow(self.ship);
+        camera.setZoom(0.8);
 }
 function addOtherPlayers(self, playerInfo) {
     const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
@@ -76,19 +102,73 @@ function addOtherPlayers(self, playerInfo) {
     otherPlayer.playerId = playerInfo.playerId;
     self.otherPlayers.add(otherPlayer);
 }
+
+function drawNode(self, str){
+    font_size = 16;
+    wrap_width = 19;
+
+    console.log(self.ship.x);
+    height = (Math.ceil(str.length/wrap_width) * 18) + 18;
+    sprite = self.add.sprite(400, 300, 'node').setDisplaySize(200, height);
+    sprite.inputEnabled = true;
+
+    style = { font: `${font_size}px Courier New`, fill: "#ff0044", wordWrap: true, wordWrapWidth: 50, align: "left", backgroundColor: "#ffff00" };
+
+    line = 0;
+    for (var i = 0; i < str.length; i += wrap_width) {
+        line++;
+        // chunks.push(str.substring(i, i + chunkSize));
+        chunk = str.substring(i, i + wrap_width);
+        // console.log(chunk);
+        text = self.add.text(310, (300-(height/2))+(line*font_size), chunk, style);
+    }
+    // this.socket.emit('newNode', {x: this.ship.x, y: this.ship.y, color: this.ship.team, text: 'hello'})
+}
+
 function update() {
     if (this.ship) {
-        if (this.cursors.left.isDown) {
-            this.ship.setAngularVelocity(-150);
-        } else if (this.cursors.right.isDown) {
-            this.ship.setAngularVelocity(150);
-        } else {
-            this.ship.setAngularVelocity(0);
+        // console.log(this.keys);
+        if (this.keys.new_node._justDown) {
+            textarea.style.display = 'block';
+        }
+        if (this.keys.create_node._justDown) {
+            textarea.style.display = 'none';
+            this.socket.emit('newNode', {x: this.ship.x, y: this.ship.y, color: this.ship.team, text: textarea.value})
         }
 
+        if (this.cursors.left.isDown) {
+            //this.ship.setAngularVelocity(-150);
+            this.ship.x -= 5
+        } 
+        
+        if (this.cursors.right.isDown) {
+            //this.ship.setAngularVelocity(150);
+            this.ship.x += 5
+        } 
+
         if (this.cursors.up.isDown) {
-            this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
-        } else {
+            //this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+            this.ship.y -= 6    
+        } 
+        
+        if (this.cursors.down.isDown) {
+            //this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+            this.ship.y += 6    
+        } 
+
+        if (this.keys.ZoomIn.isDown){
+            camera.zoom += 0.04
+            console.log("want to draw");
+           // let graphics = this.add.graphics();
+           // graphics.lineBetween(x1, y1, x2, y2);
+            //drawLine(0,0,500,500);
+        }
+        
+        if (this.keys.ZoomOut._justDown){
+            camera.zoom -= 0.04;
+        }
+
+        else {
             this.ship.setAcceleration(0);
         }
         // emit player movement
